4e9fd3ff7a28f9fb4991ac2e9ee8923b8c03de7c
Gonzalo L Petraglia <gonzalpetraglia@gmail.com>
2021-04-14 23:22:56 -0300

    style: pretty files

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -1,20 +1,20 @@
-import {HardhatRuntimeEnvironment} from 'hardhat/types';
+import { HardhatRuntimeEnvironment } from "hardhat/types";
 
-import {DeployFunction} from 'hardhat-deploy/types';
+import { DeployFunction } from "hardhat-deploy/types";
 
-const deployFunc: DeployFunction = async(hre: HardhatRuntimeEnvironment) => {
-    const { deployments, getNamedAccounts } = hre;
-    const { deployer } = await getNamedAccounts();
-    const { deploy } = deployments;
+const deployFunc: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+  const { deployments, getNamedAccounts } = hre;
+  const { deployer } = await getNamedAccounts();
+  const { deploy } = deployments;
 
-    const deployResult = await deploy('Seedifyuba', {
-      from: deployer,
-      gasLimit: 4000000,
-      args: [],
-    });
-    console.log(`Seedifyuba deployed at ${deployResult.address}`);
-    return hre.network.live; // prevents re execution on live networks
-  };
+  const deployResult = await deploy("Seedifyuba", {
+    from: deployer,
+    gasLimit: 4000000,
+    args: [],
+  });
+  console.log(`Seedifyuba deployed at ${deployResult.address}`);
+  return hre.network.live; // prevents re execution on live networks
+};
 export default deployFunc;
 
-deployFunc.id = 'deploy_seedifyuba'; // id required to prevent reexecution
+deployFunc.id = "deploy_seedifyuba"; // id required to prevent reexecution
