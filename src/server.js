a0b71c030fd873f3470e0948cc0a655d925819a7
JDSanto <javidisanto@gmail.com>
2021-07-03 17:53:52 -0300

    chore: add heroku files

--- src/server.js
+++ src/server.js
@@ -11,7 +11,8 @@ routes.forEach(route => fastify.route(route({ config, services })));
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(3000);
+    port = process.env.PORT || 3000;
+    await fastify.listen(port, "0.0.0.0");
     fastify.log.info(`server listening on ${fastify.server.address().port}`);
   } catch (err) {
     fastify.log.error(err);
