36700029a1a79a14a151dc8423cd578c4d26c85d
JDSanto <javidisanto@gmail.com>
2021-07-26 19:04:47 -0300

    fix: enable cors

--- src/server.js
+++ src/server.js
@@ -14,6 +14,11 @@ fastify.register(require("fastify-swagger"), {
   exposeRoute: true,
 });
 
+fastify.register(require("fastify-cors"), {
+  origin: "*",
+  methods: ["POST", "GET"],
+});
+
 // Declares routes
 routes.forEach(route => fastify.route(route({ config, services })));
 
